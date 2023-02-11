d8657e8b50b99460edc4410e59924bf5ddea2e6e
Jintime <wlstjd7391@gmail.com>
2023-02-12 03:25:51 +0900

    회원가입 라우팅구현

--- src/models/UserStorage.js
+++ src/models/UserStorage.js
@@ -29,6 +29,15 @@ static getUsers(...fields){
 
     return userInfo;
  }
+
+ static save(userInfo){
+    const users =this.#users;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.password.push(userInfo.password);
+    users.email.push(userInfo.email);
+    return {success:true};
+ }
 }
 
 module.exports =UserStorage;
--- src/public/js/home/register.js
+++ src/public/js/home/register.js
@@ -10,16 +10,20 @@ const id =document.querySelector("#id"),
 registerBtn.addEventListener("click",register);
 
 function register(){
+    if(!id.value)return alert("아이디를 입력해주세요.");
+    if(!password.value)return alert("비밀번호를 입력해주세요.");
+    if(!email.value)return alert("이메일을 입력해주세요.");
+    if(password.value !==confirmPassword.value)return alert("비밀번호가 일치하지 않습니다.");
+
     const req ={
         id : id.value,
         username : username.value,
         email : email.value,
         password : password.value,
-        confirmPassword : confirmPassword.value,
     };
-   console.log(req);
+
     fetch("/register",{
-        method : "post",
+        method : "POST",
         headers : {
             "Content-Type":"application/json"
         },
@@ -33,7 +37,7 @@ function register(){
             alert(res.msg);
         }
     }).catch((err)=>{
-        console.error(new Error("로그인 중 에러발생"));
+        console.error(new Error("회원가입 중 에러발생"));
     });
 
 }
