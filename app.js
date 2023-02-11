99050589c5ac01f84d57863d09d376a74eb5d746
Jintime <wlstjd7391@gmail.com>
2023-02-12 02:31:28 +0900

    User 모델구현

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 //모듈
 const express = require('express');
+const bodyParser =require("body-parser");
 const app = express();
 
 
@@ -11,6 +12,11 @@ const home = require('./src/routes/home');
 
 app.set('views','./src/views');
 app.set('view engine','ejs');
+app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+
+///url 통해 전달되는 데이터가 한글 공백 같은 문자가 포함될경우 인식되지않는 문제해결
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/',home);
 
---
+++ src/models/UserStorage.js
@@ -0,0 +1,33 @@
+"use strict";
+
+class UserStorage{
+   static #users ={
+    id : ["jin","나개발","진타임"],
+    password : ["1234","1234","123"],
+    name:["진타임","나개발","진이"],
+};
+
+static getUsers(...fields){
+    const users =this.#users;
+    const newUsers = fields.reduce((newUsers,field)=>{
+        if(users.hasOwnProperty(field)){
+            newUsers[field]=users[field];
+        }
+        return newUsers;
+    },{});
+    return newUsers;
+ }
+ 
+ static getUserInfo(id){
+    const users =this.#users;
+    const idx =users.id.indexOf(id);
+    const userInfo =Object.keys(users).reduce((newUser,info)=>{
+        newUser[info] =users[info][idx];
+        return newUser;
+    },{});
+
+    return userInfo;
+ }
+}
+
+module.exports =UserStorage;
